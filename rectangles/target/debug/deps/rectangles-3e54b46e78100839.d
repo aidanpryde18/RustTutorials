614c0d4dabc68a2388ec4fcec702686876fb89f7
William Wyatt <wwyatt@modernmktg.com>
2018-04-05 15:46:42 -0400

    Add Rectangles Program

---
+++ rectangles/target/debug/deps/rectangles-3e54b46e78100839.d
@@ -0,0 +1,5 @@
+C:\Users\wwyatt\Documents\Rust\rectangles\target\debug\deps\rectangles-3e54b46e78100839.exe: src\main.rs
+
+C:\Users\wwyatt\Documents\Rust\rectangles\target\debug\deps\rectangles-3e54b46e78100839.d: src\main.rs
+
+src\main.rs:
---
+++ rectangles/target/debug/rectangles.d
@@ -0,0 +1,1 @@
+C:\Users\wwyatt\Documents\Rust\rectangles\target\debug\rectangles.exe: C:\Users\wwyatt\Documents\Rust\rectangles\src\main.rs
